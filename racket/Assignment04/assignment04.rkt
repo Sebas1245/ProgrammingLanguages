732701edf286cb3bd20859f12e395d2e4378bca5
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-04-21 18:48:13 -0500

    Add problem descriptions and improve formatting

--- racket/Assignment04/assignment04.rkt
+++ racket/Assignment04/assignment04.rkt
@@ -26,8 +26,7 @@
             (list? (cadr bin_tree))
             (list? (car (cddr bin_tree)))
         ) (and (binary-tree? (cadr bin_tree)) (binary-tree? (car (cddr bin_tree))))) 
-        (else #f)
-))
+        (else #f)))
 
 ; problem b) 
 (define (get-minors bt val) 
@@ -35,17 +34,13 @@
         '()
         (if (> val (car bt))
             (make-bt-list bt)
-            (get-minors (cadr bt) val)
-        )
-    )
-)
+            (get-minors (cadr bt) val))))
 
 (define (make-bt-list bt)
     (if (null? bt)
         '()
-        (append (cons (car bt) (make-bt-list (cadr bt))) (make-bt-list (car (cddr bt))))
-    )
-)
+        (append (cons (car bt) (make-bt-list (cadr bt))) (make-bt-list (car (cddr bt))))))
+
 ; problem c)
 (define (longest-branch bin_tree)
     (if (null? bin_tree)
@@ -54,10 +49,7 @@
                 (length (cons (car bin_tree) (longest-branch (car (cddr bin_tree)))))
                 (length (cons (car bin_tree) (longest-branch (cadr bin_tree)))))
             (cons (car bin_tree) (longest-branch (car (cddr bin_tree))))
-            (cons (car bin_tree) (longest-branch (cadr bin_tree)))
-        )
-    )
-)
+            (cons (car bin_tree) (longest-branch (cadr bin_tree))))))
 
 ; Exercise 2: Graphs
 ; example graphs
@@ -66,87 +58,68 @@
     (B (C 9) (E 5)) 
     (C (A 12) (D 6)) 
     (D (E 7)) 
-    (E (C 3)) )
-)
+    (E (C 3)) ))
 
 (define h '( 
     (A (B 2) (D 10))
     (B (B 9) (E 5)) 
     (C (B 12) (D 6)) 
     (D (B 7)) 
-    (E (B 3)) )
-) 
+    (E (B 3)) )) 
 
 ; problem a)
 (define (destination-nodes graph node)
     (cond 
         ((null? graph) '())
         ((eq? node (caar graph)) (get-dest-nodes (cdar graph)))
-        (else (destination-nodes (cdr graph) node))
-    )
-)
+        (else (destination-nodes (cdr graph) node))))
 
 (define (get-dest-nodes adj-list)
     (if (null? adj-list)
         '()
-        (cons (caar adj-list) (get-dest-nodes (cdr adj-list)))
-    )
-)
+        (cons (caar adj-list) (get-dest-nodes (cdr adj-list)))))
 
 ; problem b)
 (define (source-nodes graph node)
     (if (null? graph) 
         '()
-        (append (get-source-nodes (cdar graph) (caar graph) node) (source-nodes (cdr graph) node)) 
-    )
-)
+        (append (get-source-nodes (cdar graph) (caar graph) node) (source-nodes (cdr graph) node))))
 
 (define (get-source-nodes adj-list parent-node node)
     (if (null? adj-list)
         '()
         (if (eq? (caar adj-list) node)
             (cons parent-node (get-source-nodes (cdr adj-list) parent-node node))
-            (get-source-nodes (cdr adj-list) parent-node node)
-        )
-    )
-)
+            (get-source-nodes (cdr adj-list) parent-node node))))
 
 ; problem c)
 (define (delete-arc graph node1 node2)
     (cond 
         ((null? (car graph)) '())
-        ((eq? (caar graph) node1) 
-            (cons (cons (caar graph) (remove-arc (cdar graph) node2)) (cdr graph))
-        )
-        (else 
-            (cons (car graph) (delete-arc (cdr graph) node1 node2))
-        )
-    )
-)
+        ((eq? (caar graph) node1) (cons (cons (caar graph) (remove-arc (cdar graph) node2)) (cdr graph)))
+        (else (cons (car graph) (delete-arc (cdr graph) node1 node2)))))
 
 (define (remove-arc adj-lst node2)
     (cond 
         ((null? adj-lst) '())
         ((eq? (caar adj-lst) node2) (remove-arc (cdr adj-lst) node2))
-        (else (cons (car adj-lst) (remove-arc (cdr adj-lst) node2)))
-    )
-)
+        (else (cons (car adj-lst) (remove-arc (cdr adj-lst) node2)))))
 
 ; Problem 3: Higher order functions
 (define mat1 '((1 2 3)(0 2 1))) 
 (define mat2 '((4 0 3 1)(5 1 2 1)(6 0 1 1)))
 
 ; problem a)
-(define (zero-count matrix)
-    (apply + (map (lambda (row)
-            (apply + (map (lambda (el) 
-                (if (zero? el) 1 0)) row))) matrix)))
 
 (define (count-zeros matrix)
   (length (apply append
                  (map (lambda (e) (if(zero? e) '(0) '()))
                       (apply append matrix)))))
-
+; alternative solution
+(define (zero-count matrix)
+    (apply + (map (lambda (row)
+            (apply + (map (lambda (el) 
+                (if (zero? el) 1 0)) row))) matrix)))
 
 ; problem b) 
 (define (minmax matrix)
@@ -158,10 +131,10 @@
 ; problem c)
 (define (multmat matrix1 matrix2)
     (map 
-        (lambda (row)
-            (map 
-                (lambda (col)
-                    (apply +
-                        (map * row col)))
-                (apply map list matrix2)))
-        matrix1))
+    (lambda (row)
+        (map 
+            (lambda (col)
+                (apply +
+                    (map * row col)))
+            (apply map list matrix2)))
+    matrix1))
